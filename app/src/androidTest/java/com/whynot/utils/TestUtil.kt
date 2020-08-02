1fc5142345f1221f1305f0dab81c403467e1f92d
SideswipeN7 <pawel.wanot@gmail.com>
2020-08-02 17:23:20 +0200

    New test util features

--- app/src/androidTest/java/com/whynot/utils/TestUtil.kt
+++ app/src/androidTest/java/com/whynot/utils/TestUtil.kt
@@ -1,6 +1,9 @@
 package com.whynot.utils
 
 import com.whynot.cookbook.db.data.Category
+import com.whynot.cookbook.db.data.Ingredient
+import com.whynot.cookbook.db.data.Recipe
+import com.whynot.cookbook.db.data.Step
 import kotlin.random.Random
 
 object TestUtil {
@@ -11,5 +14,27 @@ object TestUtil {
         name = "$categoryName ${getRandom()}"
     )
 
-    private fun getRandom():Long = random.nextLong()
+    fun createStep(synopsis: String, number: Int = 0, recipeId: Long? = null) = Step(
+        id = null,
+        recipeId = recipeId,
+        synopsis = synopsis,
+        number = number
+    )
+
+    fun createIngredient(name: String, value: String = "0", recipeId: Long? = null) = Ingredient(
+        id = null,
+        recipeId = recipeId,
+        name = name,
+        value = value
+    )
+
+    fun createRecipe(name: String, synopsis: String = "0", categoryId: Long?, image: ByteArray? = null) = Recipe(
+        id = null,
+        synopsis = synopsis,
+        name = name,
+        image = image,
+        categoryId = categoryId
+    )
+
+    private fun getRandom(): Long = random.nextLong()
 }
